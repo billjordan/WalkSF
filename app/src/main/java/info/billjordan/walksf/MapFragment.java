df128b6daef3309cde51cc84e9fe85f36b05172a
Bill Jordan <wiljor7314@students.ecpi.edu>
2015-03-09 16:58:17 -0400

    add map

--- app/src/main/java/info/billjordan/walksf/MapFragment.java
+++ app/src/main/java/info/billjordan/walksf/MapFragment.java
@@ -6,6 +6,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.mapquest.android.maps.GeoPoint;
+import com.mapquest.android.maps.MapView;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -16,6 +21,7 @@ public class MapFragment extends Fragment {
      * fragment.
      */
     private static final String ARG_SECTION_NUMBER = "section_number";
+    private Button testButton;
 
     /**
      * Returns a new instance of this fragment for the given section
@@ -36,7 +42,25 @@ public class MapFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_map, container, false);
+        // set the zoom level, center point and enable the default zoom controls
+        MapView map = (MapView) rootView.findViewById(R.id.map);
+        map.getController().setZoom(12);
+        map.getController().setCenter(new GeoPoint(37.761078, -122.446142));
+        map.setBuiltInZoomControls(true);
+        testButton = (Button) rootView.findViewById(R.id.test_button);
+        testButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "button clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
         return rootView;
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
     @Override
